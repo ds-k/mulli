321a74f3b207438334453e415a7b8dc9d2d8c8c1
Daeseong Kim <daeseongkim05@gmail.com>
2025-01-23 02:01:53 +0900

    feat: 기본 레이아웃 구조 세팅

---
+++ mulli_flutter/lib/presentation/pages/layout/main_layout.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import '../home/home_page.dart';
+import '../shop/shop_page.dart';
+import '../chat/chat_page.dart';
+import '../profile/profile_page.dart';
+
+class MainLayout extends StatefulWidget {
+  const MainLayout({super.key});
+
+  @override
+  State<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomePage(),
+    ShopPage(),
+    ChatPage(),
+    ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: '쇼핑',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: '채팅',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '프로필',
+          ),
+        ],
+      ),
+    );
+  }
+}
