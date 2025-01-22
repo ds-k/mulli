413113da309d0cc9956d34398f507c6af522e77f
Daeseong Kim <daeseongkim05@gmail.com>
2025-01-23 02:39:03 +0900

    style: bottomnavigation 스타일 적용

--- mulli_flutter/lib/presentation/pages/layout/main_layout.dart
+++ mulli_flutter/lib/presentation/pages/layout/main_layout.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../../../constant/colors.dart';
 import '../home/home_page.dart';
 import '../shop/shop_page.dart';
 import '../chat/chat_page.dart';
@@ -31,28 +33,62 @@ class _MainLayoutState extends State<MainLayout> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '홈',
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: AppColors.lightGray,
+              width: 1,
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag),
-            label: '쇼핑',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: '채팅',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '프로필',
-          ),
-        ],
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/bn_icons/home.svg',
+                colorFilter: ColorFilter.mode(
+                  _selectedIndex == 0 ? AppColors.black : AppColors.darkGray,
+                  BlendMode.srcIn,
+                ),
+              ),
+              label: '홈',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/bn_icons/shop.svg',
+                colorFilter: ColorFilter.mode(
+                  _selectedIndex == 1 ? AppColors.black : AppColors.darkGray,
+                  BlendMode.srcIn,
+                ),
+              ),
+              label: '쇼핑',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/bn_icons/chat.svg',
+                colorFilter: ColorFilter.mode(
+                  _selectedIndex == 2 ? AppColors.black : AppColors.darkGray,
+                  BlendMode.srcIn,
+                ),
+              ),
+              label: '채팅',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/bn_icons/profile.svg',
+                colorFilter: ColorFilter.mode(
+                  _selectedIndex == 3 ? AppColors.black : AppColors.darkGray,
+                  BlendMode.srcIn,
+                ),
+              ),
+              label: '프로필',
+            ),
+          ],
+        ),
       ),
     );
   }
