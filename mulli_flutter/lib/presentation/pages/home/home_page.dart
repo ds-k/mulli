609ef2459071f3e0dcaba973f314ec4afd225633
Daeseong Kim <daeseongkim05@gmail.com>
2025-01-23 02:02:13 +0900

    feat: 서버팟 연동 테스트 코드 홈으로 이동

---
+++ mulli_flutter/lib/presentation/pages/home/home_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:mulli_client/mulli_client.dart';
+import 'package:serverpod_flutter/serverpod_flutter.dart';
+
+var client = Client('http://$localhost:8080/')
+  ..connectivityMonitor = FlutterConnectivityMonitor();
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? _resultMessage;
+  String? _errorMessage;
+  final _textEditingController = TextEditingController();
+
+  void _callHello() async {
+    try {
+      final result = await client.example.hello(_textEditingController.text);
+      setState(() {
+        _errorMessage = null;
+        _resultMessage = result;
+      });
+    } catch (e) {
+      setState(() {
+        _errorMessage = '$e';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('홈'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: TextField(
+                controller: _textEditingController,
+                decoration: const InputDecoration(
+                  hintText: '이름을 입력하세요',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: ElevatedButton(
+                onPressed: _callHello,
+                child: const Text('서버로 전송'),
+              ),
+            ),
+            _ResultDisplay(
+              resultMessage: _resultMessage,
+              errorMessage: _errorMessage,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ResultDisplay extends StatelessWidget {
+  final String? resultMessage;
+  final String? errorMessage;
+
+  const _ResultDisplay({
+    this.resultMessage,
+    this.errorMessage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    String text;
+    Color backgroundColor;
+    if (errorMessage != null) {
+      backgroundColor = Colors.red[300]!;
+      text = errorMessage!;
+    } else if (resultMessage != null) {
+      backgroundColor = Colors.green[300]!;
+      text = resultMessage!;
+    } else {
+      backgroundColor = Colors.grey[300]!;
+      text = '서버 응답을 기다리는 중...';
+    }
+
+    return Container(
+      height: 50,
+      color: backgroundColor,
+      child: Center(
+        child: Text(text),
+      ),
+    );
+  }
+}
