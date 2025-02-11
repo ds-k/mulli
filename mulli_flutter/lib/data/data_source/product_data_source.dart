5a34ba321167e282472f858de29fd9a7a5411a5a
Daeseong Kim <daeseongkim05@gmail.com>
2025-02-11 18:09:57 +0900

    feat: 아키텍쳐 변경으로 인한 디렉토리 구조 변경 (클린아키텍쳐 미사용, 서버팟에 모든 서버 역할을 맡기고, 플러터 앱에서는 UI/UX 및 상태관리에만 집중)

--- mulli_flutter/lib/data/data_source/product_data_source.dart
+++
@@ -1,5 +0,0 @@
-import 'package:mulli_client/mulli_client.dart';
-
-abstract class ProductDataSource {
-  Future<void> createProduct(Products product);
-}
--- mulli_flutter/lib/domain/repository/product_repository.dart
+++
@@ -1,5 +0,0 @@
-// abstract class ProductRepository {
-//   Future<void> createProduct(Product product);
-//   Future<List<Product>> getProducts();
-//   Future<Product?> getProductById(int id);
-// }
